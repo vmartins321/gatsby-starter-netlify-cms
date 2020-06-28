c20abf7a2076e85bea6824c5ddfaf32737058561
vmartins321 <7216801+vmartins321@users.noreply.github.com>
2020-06-28 08:10:14 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,23 +60,23 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
+              /*<Link className="navbar-item" to="/about">
                 About
               </Link>
               <Link className="navbar-item" to="/products">
                 Products
-              </Link>
+              </Link>*/
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              /*<Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+              </Link>*/
             </div>
-            <div className="navbar-end has-text-centered">
+            /*<div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
@@ -87,7 +87,7 @@ const Navbar = class extends React.Component {
                   <img src={github} alt="Github" />
                 </span>
               </a>
-            </div>
+            </div>*/
           </div>
         </div>
       </nav>
